fc870b01bca150ef2778b9f31d399ade72e721f8
Ábrahám Szigethy <55062985+AbThy@users.noreply.github.com>
2022-04-10 21:30:39 +0200

    init

---
+++ app/src/main/java/h052104/gaiaestate/MainActivity.java
@@ -0,0 +1,14 @@
+package h052104.gaiaestate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    } 
+}
