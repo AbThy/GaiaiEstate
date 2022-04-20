2c1e662f40ad94def83495683f0435c6bb996ac5
Ábrahám Szigethy <55062985+AbThy@users.noreply.github.com>
2022-04-20 19:40:25 +0200

    login-register_design
    -Added firebase
    -Created xml's for Login and Register activity
    -Created some string and value resources
    -Created empty functions related to login and register activity
    -Register activity is now openable

--- app/src/main/java/h052104/gaiaestate/MainActivity.java
+++ app/src/main/java/h052104/gaiaestate/MainActivity.java
@@ -2,13 +2,39 @@ package h052104.gaiaestate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } 
+    }
+
+    // Button events
+    public void googleLogin(View view) {
+        // TODO
+    }
+
+    public void login(View view) {
+        String userName = ((EditText)findViewById(R.id.editTextUserName)).getText().toString();
+        String password = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
+        String logMessage = "User login: " + userName + ", with password: " + password + " .";
+        Log.i(LOG_TAG, logMessage);
+    }
+
+    public void loginAsAnon(View view) {
+        // TODO
+    }
+
+    public void openRegisterActivity(View view) {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
 }
