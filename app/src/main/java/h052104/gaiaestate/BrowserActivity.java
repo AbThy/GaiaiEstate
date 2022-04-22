999415fac408de3a967c6b875df90765e4e82441
Ábrahám Szigethy <55062985+AbThy@users.noreply.github.com>
2022-04-22 18:07:38 +0200

    firebaseRegister
    -Registration with FirebaseAuth
    -Toast messages
    -BrowserActivity init

---
+++ app/src/main/java/h052104/gaiaestate/BrowserActivity.java
@@ -0,0 +1,45 @@
+package h052104.gaiaestate;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import h052104.gaiaestate.ui.main.SectionsPagerAdapter;
+import h052104.gaiaestate.databinding.ActivityBrowserBinding;
+
+public class BrowserActivity extends AppCompatActivity {
+
+    private ActivityBrowserBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityBrowserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
--- app/src/main/java/h052104/gaiaestate/RegisterActivity.java
+++ app/src/main/java/h052104/gaiaestate/RegisterActivity.java
@@ -1,21 +1,74 @@
 package h052104.gaiaestate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private static final String LOG_TAG = RegisterActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     public void cancel(View view) {
+
     }
 
     public void register(View view) {
+        String uName = ((EditText)findViewById(R.id.editTextUserName)).getText().toString().trim(); //TRIM!
+        String email = ((EditText)findViewById(R.id.editTextEmail)).getText().toString().trim();    //TRIM!
+        String pass = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
+        String passCheck = ((EditText)findViewById(R.id.editTextPasswordAgain)).getText().toString();
+        CheckBox termsAcceptedCheckBox = findViewById(R.id.termsCheckBox);
+
+        if(termsAcceptedCheckBox.isChecked() == false){
+            Log.i(LOG_TAG, "Terms of use not checked!");
+            Toast.makeText(RegisterActivity.this, "Please read and accept the terms of use!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!pass.equals(passCheck)) {
+            Log.i(LOG_TAG, "Passwords do not match!");
+            Toast.makeText(RegisterActivity.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.i(LOG_TAG, "Firebase task: createUserWithEmailAndPassword succeeded!");
+                    startApp();
+                } else {
+                    Log.d(LOG_TAG, "Error: firebaseAuth.createUserWithEmailAndPassword");
+                    Toast.makeText(RegisterActivity.this, "!! Firebase task: createUserWithEmailAndPassword failed: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public void startApp(){
+        Intent i = new Intent(this, BrowserActivity.class);
+        startActivity(i);
     }
 }
